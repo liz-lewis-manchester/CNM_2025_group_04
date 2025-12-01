8f725808f0f3ec84f86b02b9db4a32aa6a1b3218
avetisian1 <aleksavet9@gmail.com>
2025-12-01 18:53:12 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import numpy as np
+
+def read_initial_conditions(path):
+    """
+    Reads initial pollutant concentration data from a CSV file.
+
+    Expected CSV columns:
+        x  - position along the river (m)
+        C  - concentration (µg/m³)
+    """
+    return pd.read_csv(path)
+
+def interpolate_to_grid(df, grid_x):
+    """
+    Placeholder for interpolation of CSV data onto the model grid.
+
+    Parameters:
+        df      : DataFrame with columns ['x', 'C']
+        grid_x  : 1D numpy array of model grid positions
+
+    Returns:
+        1D numpy array of interpolated concentrations on grid_x.
+    """
+    # To be implemented by Initial Conditions Lead
+    pass
 
