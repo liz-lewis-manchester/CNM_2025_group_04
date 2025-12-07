961db784dd69208d3e0cebcb83e3646d4b3a225a
avetisian1 <aleksavet9@gmail.com>
2025-12-07 17:13:00 +0000

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,16 +1,1 @@
-import numpy as np
-from src.model import solve_advection
-
-def test_solver_runs_with_simple_input():
-    nx = 5
-    C0 = np.zeros(nx)
-    C0[0] = 1.0
-    U = 0.1
-    dx = 0.2
-    dt = 0.1
-    nt = 3
-
-    C = solve_advection(C0, U, dx, dt, nt)
-
-    assert C.shape == (nt, nx)
 
