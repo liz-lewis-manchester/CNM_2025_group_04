b6dcbe9e14449e4ff616692850cfba2d09cd2cfa
reggie-me27 <reggiemessenger6@gmail.com>
2025-12-07 15:51:35 +0000

    Create test_case_3.py
    Test Case 3 (Sensitivity to U, dx, and dt)

---
+++ src/tests/test_case_3.py
@@ -0,0 +1,47 @@
+# test_case_3 - Sensitivity Testing Code
+
+import numpy as np
+
+def sensitivity_tests(U_values, dx_values, dt_values, L = 20, t_end = 300):
+
+  results = {}
+
+  for U in U_values:
+    for dx in dx_values:
+      for dt in dt_values:
+
+        # Creates grids
+        x = create_space_grid(0, L, dx)
+        t = create_time_grid(0, t_end, dt)
+
+        nx = x.size
+        nt = t.size
+      
+        # Initial Conditions
+        C0 = np.zeros(nx)
+        C0[0] = 250
+
+        # Inlet Boundary Conditions
+        def inlet(time):
+          return 250
+
+        # Run PDE Solver
+        C = advect_1d_backward(C0, U, dx, dt, nt, inlet_func=inlet)
+
+        # Update results dictionary
+        results[(U, dx, dt)] = {'x': x, 't' : t, 'C' : C}
+
+        print('U =', {U}, 'dx=', {dx}, 'dt=', {dt}, 'nx=', {nx}, 'nt=', {nt})
+        
+  return results
+
+# Input parameters to be tested here:
+U_values  = [0.05, 0.1, 0.2]
+dx_values = [0.1, 0.2, 0.5]
+dt_values = [1.0, 5.0, 10.0]
+
+results = sensitivity_tests(U_values, dx_values, dt_values)
+
+  
+
+    
